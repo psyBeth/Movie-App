e5f647d80f826fb99cdaa0cc75d262c814cc1d86
Betul <betulkoru06@gmail.com>
2024-03-31 01:04:28 +0300

    example display name (no API yet)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,12 @@ function classNames(...classes) {
 }
 
 export default function Example() {
+  const currentUser = { displayName: "Betul Koru"};
+
   return (
-    <Disclosure as="nav" className="bg-neutral-100 dark:bg-gray-900 py-3 dark: text-white fixed w-full z-20 top-0">
+    <Disclosure as="nav" className="bg-neutral-100 text-black dark:bg-gray-900 py-3 dark: text-white fixed w-full z-20 top-0">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
+        <div className="relative flex items-center justify-between">
           <Link className='pr-2 text-2xl font-semibold' to="/" >  
           {/* left side content */}
             React Movie App
@@ -18,8 +20,9 @@ export default function Example() {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
-
+            <h5 className='mr-2 capitalize'>
+              {currentUser?.displayName}
+            </h5>
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
