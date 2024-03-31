27897866a75d32b181cbe8de840f8e8cb4bbb877
Betul <betulkoru06@gmail.com>
2024-04-01 01:23:55 +0300

    auth context built

---
+++ src/context/AuthContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export const AuthContext = createContext();
+// const { Provider } = createContext();
+
+//* custom hook 
+export const useAuthContext = () => {
+    return useContext(AuthContext);
+};
+
+const AuthContextProvider = ({ children }) => {
+    const values = {};
+
+  return (
+    <AuthContext.Provider value={values}>
+        { children }
+    </AuthContext.Provider>
+  )
+};
+
+export default AuthContextProviderProvider;
