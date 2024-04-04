7fb94151d7e66743e9d094760b3f1fbe39f0f129
Betul <betulkoru06@gmail.com>
2024-04-04 03:57:48 +0300

    movie context ready

---
+++ src/context/MovieContext.js
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react'
+
+export const MovieContext = createContext();
+
+export const useMovieContext = () => {
+    return useContext(MovieContext);
+};
+
+const MovieContextProvider = ({ children }) => {
+    return (
+        <MovieContext.Provider value={ null }>
+            {children}
+        </MovieContext.Provider>
+    )
+}
+
+export default MovieContextProvider;
