d6d0aed14a7eada22285b68b654a82e7c0b0b699
Betul <betulkoru06@gmail.com>
2024-04-04 05:33:22 +0300

    movie context 1

--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -1,4 +1,6 @@
-import React, { createContext } from 'react'
+import React, { createContext } from 'react';
+import axios from "axios";
+
 
 export const MovieContext = createContext();
 
@@ -6,12 +8,24 @@ export const useMovieContext = () => {
     return useContext(MovieContext);
 };
 
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
+
 const MovieContextProvider = ({ children }) => {
+
+    const getMovies = () => {
+        axios
+            .get(FEATURED_API)
+            .then((res) =>console.log(res))
+            .catch((err) => console.log(err))
+    }
+
     return (
         <MovieContext.Provider value={ null }>
             {children}
         </MovieContext.Provider>
-    )
-}
+    );
+};
 
 export default MovieContextProvider;
