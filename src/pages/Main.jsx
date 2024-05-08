35ae8f1e25127891f95c0bd5635166da7912a725
Betul <betulkoru06@gmail.com>
2024-05-08 21:19:21 +0300

    tiny edit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useMovieContext } from "../context/MovieContext";
 import MovieCard from "../components/MovieCard";
 import { toastWarnNotify } from "../helpers/ToastNotify";
