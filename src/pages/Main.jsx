d70f02dca79c358cac483e48b9ecc6dbfceab6d3
Betul <betulkoru06@gmail.com>
2024-05-08 21:16:25 +0300

    search feature in main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import { useMovieContext } from '../context/MovieContext';
-import MovieCard from '../components/MovieCard';
+import React, { useRef, useState } from "react";
+import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
+import { toastWarnNotify } from "../helpers/ToastNotify";
+import { useAuthContext } from "../context/AuthContext";
 
 const Main = () => {
-  const { movies, loading } = useMovieContext(); 
+  const { movies, loading, getMovies } = useMovieContext();
+
+  const { currentUser } = useAuthContext();
+
+  //* hook that let us reach the DOM elements
+  const inputRef = useRef();
+
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  // const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`;
+  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputRef.current.value && currentUser) {
+      getMovies(SEARCH_API + inputRef.current.value);
+    } else if (!currentUser) {
+      toastWarnNotify("Please log in to search a movie.");
+    } else {
+      toastWarnNotify("Please enter a text.");
+    }
+  };
+
   return (
     <div className='dark:bg-gray-dark-main'>
+      <form className="flex justify-center p-2" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="w-80 h-8 rounded-md p-1 m-2"
+          placeholder="Search a movie..."
+          // onChange={(e) => setSearch(e.target.value)}
+          ref={inputRef}
+        />
+        <button className="btn-danger-bordered" type="submit">
+          Search
+        </button>
+      </form>
       <div className='flex flex-wrap justify-center'>
-      {loading ? (
+        {loading ? (
           <div
             className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
             role="status"
