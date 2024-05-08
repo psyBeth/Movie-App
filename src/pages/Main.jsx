34f051d76dd3f165ce7fc23dc187d927807ee486
Betul <betulkoru06@gmail.com>
2024-05-08 20:33:23 +0300

    movie card built and added to main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useMovieContext } from '../context/MovieContext';
+import MovieCard from '../components/MovieCard';
 
 const Main = () => {
+  const { movies, loading } = useMovieContext(); 
   return (
     <div className='dark:bg-gray-dark-main'>
-      MAIN
+      <div className='flex flex-wrap justify-center'>
+      {loading ? (
+          <div
+            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+        )}
+      </div>
     </div>
   )
 }
