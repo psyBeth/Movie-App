7084bb09d739f907579a28add281c4e2972c59d9
Betul <betulkoru06@gmail.com>
2024-05-08 20:57:10 +0300

    private router to movie details

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Main from "../pages/Main";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
 import Navbar from '../components/Navbar';
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
@@ -12,13 +13,15 @@ const AppRouter = () => {
     // <BrowserRouter>
     <>
       <Navbar />
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/details/:id" element={<MovieDetail />} />
-        </Routes>
-        </>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="" element={<PrivateRouter />}>
+          <Route path="/details/:id" element={<MovieDetail />} />
+        </Route>
+      </Routes>
+    </>
     // </BrowserRouter>
   )
 };
